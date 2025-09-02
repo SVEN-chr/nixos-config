3749d35e4ffe286194f8f5cc52bc2225e4d34511
sven <chr1768902202@gmail.com>
2025-09-03 02:10:47 +0800

    我的nixos配置

---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  description = "A simple NixOS flake";
+
+  inputs = {
+    nixpkgs.url = "https://mirrors.ustc.edu.cn/nix-channels/nixpkgs-unstable/nixexprs.tar.xz";
+    home-manager = {
+      # url = "http://hk-yd-proxy.gitwarp.com:6699/https://github.com/nix-community/home-manager/archive/54b2879ce622d44415e727905925e21b8f833a98.tar.gz";
+      # The `follows` keyword in inputs is used for inheritance.
+      # Here, `inputs.nixpkgs` of home-manager is kept consistent with
+      # the `inputs.nixpkgs` of the current flake,
+      # to avoid problems caused by different versions of nixpkgs.
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
+    nixosConfigurations.sven = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        # 这里导入之前我们使用的 configuration.nix，
+        # 这样旧的配置文件仍然能生效
+        ./configuration.nix
+                 # 将 home-manager 配置为 nixos 的一个 module
+          # 这样在 nixos-rebuild switch 时，home-manager 配置也会被自动部署
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+
+            # 这里的 ryan 也得替换成你的用户名
+            # 这里的 import 函数在前面 Nix 语法中介绍过了，不再赘述
+            home-manager.users.chr = import ./home.nix;
+
+            # 使用 home-manager.extraSpecialArgs 自定义传递给 ./home.nix 的参数
+            # 取消注释下面这一行，就可以在 home.nix 中使用 flake 的所有 inputs 参数了
+            # home-manager.extraSpecialArgs = inputs;
+          }
+      ];
+    };
+  };
+}
